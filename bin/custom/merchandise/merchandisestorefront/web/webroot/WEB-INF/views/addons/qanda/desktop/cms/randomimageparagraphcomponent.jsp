d4088a6e377216a81cc2efab57c25a27b8274c67
Maksym Baboshyn <m.baboshyn@aimprosoft.com>
2015-10-26 11:46:55 +0200

    source files

---
+++ bin/custom/merchandise/merchandisestorefront/web/webroot/WEB-INF/views/addons/qanda/desktop/cms/randomimageparagraphcomponent.jsp
@@ -0,0 +1,5 @@
+<body>
+<h1>This is sample widget view</h1>
+<h2>Sample Q&A Widget without TurnTo Services</h2>
+<img src="http://lorempixel.com/${width}/${height}/${topic}/${text}" />
+</body>
---
+++ bin/custom/qanda/gensrc/com/aimprosoft/platform/addons/qanda/constants/GeneratedQandaConstants.java
@@ -0,0 +1,50 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Oct 23, 2015 3:25:14 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.aimprosoft.platform.addons.qanda.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedQandaConstants
+{
+	public static final String EXTENSIONNAME = "qanda";
+	public static class TC
+	{
+		public static final String RANDOMIMAGEPARAGRAPHCOMPONENT = "RandomImageParagraphComponent".intern();
+		public static final String RANDOMPICTURETOPIC = "RandomPictureTopic".intern();
+	}
+	public static class Attributes
+	{
+		// no constants defined.
+	}
+	public static class Enumerations
+	{
+		public static class RandomPictureTopic
+		{
+			public static final String FASHION = "fashion".intern();
+			public static final String SPORTS = "sports".intern();
+			public static final String ABSTRACT = "abstract".intern();
+			public static final String ANIMALS = "animals".intern();
+			public static final String CITY = "city".intern();
+			public static final String FOOD = "food".intern();
+			public static final String NIGHTLIFE = "nightlife".intern();
+			public static final String PEOPLE = "people".intern();
+			public static final String NATURE = "nature".intern();
+			public static final String TECHNICS = "technics".intern();
+			public static final String TRANSPORT = "transport".intern();
+		}
+	}
+	
+	protected GeneratedQandaConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
