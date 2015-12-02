534807ba861475b73c3b06b6aa8f4701a5ae30c3
akorneyko27 <alexandr.korneyko@gmail.com>
2015-12-02 18:58:34 +0200

    backoffice config

--- bin/custom/turntobackoffice/backoffice/src/com/hybris/turntobackoffice/widgets/TurntobackofficeController.java
+++ bin/custom/turntobackoffice/backoffice/src/com/hybris/turntobackoffice/widgets/TurntobackofficeController.java
@@ -14,10 +14,12 @@ package com.hybris.turntobackoffice.widgets;
 import com.hybris.cockpitng.annotations.ViewEvent;
 import com.hybris.cockpitng.util.DefaultWidgetController;
 import com.hybris.turntobackoffice.services.TurntobackofficeService;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Selectbox;
 
 
 public class TurntobackofficeController extends DefaultWidgetController {
@@ -25,16 +27,53 @@ public class TurntobackofficeController extends DefaultWidgetController {
 
     private Checkbox checkbox;
 
+    private Checkbox turnQA;
+    private Checkbox turnRating;
+
+
+    private Selectbox selectQAMode;
+    private Selectbox selectRatingMode;
+
     @WireVariable
     private TurntobackofficeService turntobackofficeService;
+
+
+    @Override
+    public void initialize(Component comp) {
+        super.initialize(comp);
+        selectQAMode.setDisabled(true);
+        selectRatingMode.setDisabled(true);
+    }
 
     @ViewEvent(componentID = "sendFeedBtn", eventName = Events.ON_CLICK)
     public void sendProducts() throws Exception {
         Messagebox.show(turntobackofficeService.sendCatalogFeed());
     }
 
-    @ViewEvent(componentID = "turnToSwitcher", eventName = Events.ON_CHANGE)
-    public void asd() throws InterruptedException {
+    @ViewEvent(componentID = "turnQA", eventName = Events.ON_CHECK)
+    public void turnQA() throws InterruptedException {
+        selectQAMode.setDisabled(!turnQA.isChecked());
+        Messagebox.show(turntobackofficeService.getHelloWorld());
+    }
+
+    @ViewEvent(componentID = "turnRating", eventName = Events.ON_CHECK)
+    public void turnRating() throws InterruptedException {
+        selectRatingMode.setDisabled(!turnRating.isChecked());
+        Messagebox.show(turntobackofficeService.getHelloWorld());
+    }
+
+    @ViewEvent(componentID = "turnOrderReporting", eventName = Events.ON_CHECK)
+    public void turnOrderReporting() throws InterruptedException {
+        Messagebox.show(turntobackofficeService.getHelloWorld());
+    }
+
+    @ViewEvent(componentID = "selectQAMode", eventName = Events.ON_SELECT)
+    public void selectQAMode() throws InterruptedException {
+        Messagebox.show(turntobackofficeService.getHelloWorld());
+    }
+
+    @ViewEvent(componentID = "selectRatingMode", eventName = Events.ON_SELECT)
+    public void selectRatingMode() throws InterruptedException {
         Messagebox.show(turntobackofficeService.getHelloWorld());
     }
 }
